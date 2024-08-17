97bcf30f9d95def1812af4687bcea9c9b53e2cdd
Philipp Wischnewski <phil-wisch@web.de>
2024-08-17 13:31:55 +0200

    Added Game Functionality

---
+++ game.js
@@ -0,0 +1,97 @@
+// every field of the tic tac toe board is numbered/indexed from top left to bottom right. There are nine fields in total.
+// the numbering goes like this:
+// 1, 2, 3
+// 4, 5, 6
+// 7, 8, 9
+
+class Game {
+    static winConditions = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+
+        [1, 5, 9],
+        [3, 5, 7]
+    ]
+
+    constructor(startingPlayer) {
+        // keeps track of where players put "X" and "O". Looks like: {1: "X", 7: "O", 2: "X"}
+        this.gameState = {};
+        // if the starting player is not provided or is not 'X' or 'O', it picks a random player as a fallback
+        this.playerTurn = (startingPlayer === 'X' || startingPlayer === 'O') ? startingPlayer : this.pickRandomPlayer();
+    }
+
+    takeTurn(player, fieldIndex) {
+        let valid, gameOver, winner;
+
+        valid = this.isValidTurn(player, fieldIndex);
+
+        if (valid) {
+            this.gameState[fieldIndex] = player;
+            winner = this.checkForWinner();
+
+            if (winner) {
+                gameOver = true;
+            } else {
+                gameOver = this.isGameboardFull();
+            }
+
+            // after player 1 takes their turn, it's player 2's turn
+            this.playerTurn = player === 'X' ? 'O' : 'X';
+        }
+
+        return {
+            valid,
+            gameOver,
+            winner
+        }
+    }
+
+    checkForWinner() {
+        for (const condition of Game.winConditions) {
+            let xCount = 0;
+            let oCount = 0;
+
+            // count who many "X"s and "O"s there are. If there are 3 of one, that player wins.
+            // ie the first wincondition it checks for is [1,2,3], which is 3 in-a-row in the first row.
+            // the check for [1,5,9] would be diagonal from top left to bottom right
+            for (const index of condition) {
+                if (this.gameState[index] === 'X') xCount++;
+                else if (this.gameState[index] === 'O') oCount++;
+
+                if (xCount === 3) return 'X';
+                else if (oCount === 3) return 'O';
+            }
+        }
+
+        return null;
+    }
+
+    isGameboardFull() {
+        return this.gameState.size === 9;
+    }
+
+    isValidTurn(player, fieldIndex) {
+        // if the player is not assigned either 'O' or 'X'
+        if (!(player === 'X' || player === 'O')) return false;
+        // if it's not that player's turn
+        if (this.playerTurn !== player) return false;
+        // if the field has already been taken by a player, it can't be taken anymore
+        if (this.gameState[fieldIndex]) return false;
+        // if the field's index is outside the field (outside 1-9)
+        if (fieldIndex < 1 || fieldIndex > 9) return false;
+
+        return true;
+    }
+
+    pickRandomPlayer() {
+        return Math.random() < 0.5 ? "X" : "O";
+    }
+
+}
+
+module.exports = Game;
